1154be53c0dbff74d93701d245d04cdbe3ab561c
Muhammadali7866 <malikali8157@gmail.com>
2025-05-27 18:55:33 +0500

    animation till technologies done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,45 @@
 import React from 'react'
 import profilePic from "../assets/raviKumarProfile.webp"
 import {HERO_CONTENT} from "../constants/"
+import { motion } from 'framer-motion'
+const containerVarient = {
+  hidden:{opacity:0,x:-100},
+  visible:{
+    opacity:1,
+    x:0,
+    transition:{
+      duration:0.5,
+      staggerChildren:0.5,
+    }
+  }
+}
+const childVarients = {
+  hidden:{opacity:0,x:-100},
+  visible:{opacity:1,x:0,transition:{duration:0.5}}
+}
 export const Hero = () => {
   return (
     <div className='pb-4 lg:mb-36'>
         <div className='flex flex-wrap lg:flex-row-reverse'>
             <div className='w-full lg:w-1/2'>
                <div className='flex justify-center lg:p-8'>
-                <img src={profilePic} alt="Profile picture" className='border border-stone-900 rounded-3xl' /> 
+                <motion.img src={profilePic} alt="Profile picture" className='border border-stone-900 rounded-3xl'
+                initial={{x:100,opacity:0}}
+                animate= {{x:0,opacity:1}}
+                width={650}
+                height={650}
+                transition={{duration:1,delay:1.5}}
+                /> 
                </div>
               
             </div>
             <div className='w-full lg:w-1/2'>
-            <div className='flex flex-col items-center lg:items-start mt-10'>
-                <h2 className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Usama Ahmad</h2>
-                <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</span>
-                <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>{HERO_CONTENT}</p>
-                <a href="/" target='_blank' rel='noopener noreferrer' download className='bg-white rounded-full p-4 text-sm text-stone-700 mb-10'>Download Resume</a>
-            </div>
+            <motion.div initial='hidden' animate='visible' variants={containerVarient} className='flex flex-col items-center lg:items-start mt-10'>
+                <motion.h2 variants={childVarients} className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Usama Ahmad</motion.h2>
+                <motion.span variants={childVarients}  className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</motion.span>
+                <motion.p variants={childVarients} className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>{HERO_CONTENT}</motion.p>
+                <motion.a variants={childVarients} href="/" target='_blank' rel='noopener noreferrer' download className='bg-white rounded-full p-4 text-sm text-stone-700 mb-10'>Download Resume</motion.a>
+            </motion.div>
             </div>
 
         </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 import { BiLogoPostgresql } from 'react-icons/bi'
 import { DiRedis } from 'react-icons/di'
@@ -5,40 +6,51 @@ import { FaNodeJs } from 'react-icons/fa'
 import { RiReactjsLine } from 'react-icons/ri'
 import { SiMongodb, SiMysql, SiPython } from 'react-icons/si'
 import { TbBrandNextjs } from 'react-icons/tb'
-
+const iconVarients = (duration)=>({
+    initial:{y:-10},
+    animate:{
+        y:[10,-10],
+        transition:{
+            duration:duration,
+            ease:"linear",
+            repeat:Infinity,
+            repeatType:"reverse"
+        }
+    }
+})
 const Technologies = () => {
   return (
     <div className='pb-24'>
-        <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-        <div className='flex justify-center items-center flex-wrap gap-4'>
-            <div>
+        <motion.h2 whileInView={{opacity:1,y:0}} initial={{opacity:0,y:-100}} transition={{duration:1.5}} className='my-20 text-center text-4xl'>Technologies</motion.h2>
+        <motion.div whileInView={{opacity:1,y:0}} initial={{opacity:0,x:-100}} transition={{duration:1.5}} className='flex justify-center ml-20 items-center flex-wrap gap-4 w-full'>
+            <motion.div initial='initial' animate="animate" variants={iconVarients(2.5)}>
                 <RiReactjsLine className='text-7xl text-cyan-400' />
-            </div>
-            <div className='p-4'>
-                <TbBrandNextjs className='text-7xl' />
-            </div>
-            <div className='p-4'>
+            </motion.div>
+            <motion.div initial='initial' animate="animate" variants={iconVarients(2.5)} className='p-4'>
+                <TbBrandNextjs className='text-7xl ' />
+            </motion.div>
+            <motion.div className='p-4' initial='initial' animate="animate" variants={iconVarients(3)}>
                 <SiMongodb className='text-7xl text-cyan-500' />
-            </div>
-            <div className='p-4'>
+            </motion.div>
+            <motion.div className='p-4' initial='initial' animate="animate" variants={iconVarients(5)}>
                 <DiRedis className='text-7xl text-red-700' />
-            </div>
-             <div className='p-4'>
+            </motion.div>
+             <motion.div className='p-4' initial='initial' animate="animate" variants={iconVarients(2.5)}>
                 <FaNodeJs className='text-7xl text-green-500' />
-            </div>
-              <div className='p-4'>
+            </motion.div>
+              <motion.div className='p-4' initial='initial' animate="animate" variants={iconVarients(2)}>
                 <BiLogoPostgresql className='text-7xl text-sky-700' />
-            </div>
-            <div className='p-4'>
+            </motion.div>
+            <motion.div className='p-4' initial='initial' animate="animate" variants={iconVarients(26)}>
                   <SiMysql className="text-7xl text-blue-600" />
 
-            </div>
-            <div className='p-4'>
+            </motion.div>
+            <motion.div className='p-4' initial='initial' animate="animate" variants={iconVarients(4)}>
                   <SiPython className="text-7xl text-yellow-500" />
 
-            </div>
+            </motion.div>
 
-        </div>
+        </motion.div>
     </div>
   )
 }
