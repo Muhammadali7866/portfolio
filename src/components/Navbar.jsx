0d275306c68ee6856a6c1ba325a3dab310969cd6
MuhammadAli7866 <malikali8157@gmail.com>
2025-08-02 14:20:43 +0500

    m-ali update social links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FaLinkedin, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { SiLeetcode, SiHackerrank } from "react-icons/si";
 import logo from "../assets/raviKumarLogo.webp"
 export const Navbar = () => {
   return (
@@ -8,21 +9,16 @@ export const Navbar = () => {
         <a href="/" aria-label='Home' ><img src={logo} alt="Logo" height={33} width={50} /></a>
       </div>
       <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <a href="" target='_blank' rel='noopener norefernce' aria-label='Linkdin'>
+        <a href="https://www.linkedin.com/in/muhammadslinkdin" target='_blank' rel='noopener noreferrer' aria-label='LinkedIn'>
           <FaLinkedin />
         </a>
-        <a href="" target='_blank' rel='noopener norefernce' aria-label='Linkdin'>
+        <a href="https://www.github.com/Muhammadali7866" target='_blank' rel='noopener noreferrer' aria-label='GitHub'>
           <FaGithub />
         </a>
 
-        <a href="" target='_blank' rel='noopener norefernce' aria-label='Linkdin'>
-          <FaInstagram />
+        <a href="https://www.leetcode.com/u/malikali8157/" target='_blank' rel='noopener noreferrer' aria-label='LeetCode'>
+          <SiLeetcode />
         </a>
-
-        <a href="" target='_blank' rel='noopener norefernce' aria-label='Linkdin'>
-          <FaTwitter />
-        </a>
-
 
       </div>
     </nav>
